5587ec36cc7d9f4ce202a7084775f8698c3a0595
Anas Salman <asalman@souq.com>
2015-12-30 00:59:38 +0200

    + add board services

---
+++ app/src/main/java/com/asalman/trellodoro/restapi/service/BoardServices.java
@@ -0,0 +1,18 @@
+package com.asalman.trellodoro.restapi.service;
+
+
+import com.asalman.trellodoro.models.Board;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+
+/**
+ * Created by asalman on 12/30/15.
+ */
+public interface BoardServices {
+
+    @GET("/members/me/boards")
+    public void getAllBoards(Callback<List<Board>> response);
+}
--- app/src/main/java/com/asalman/trellodoro/ui/activities/MainActivity.java
+++ app/src/main/java/com/asalman/trellodoro/ui/activities/MainActivity.java
@@ -2,14 +2,37 @@ package com.asalman.trellodoro.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.asalman.trellodoro.R;
+import com.asalman.trellodoro.models.Board;
+import com.asalman.trellodoro.restapi.RestClient;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class MainActivity extends AppCompatActivity implements Callback<List<Board>> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RestClient.getBoardServices().getAllBoards(this);
+    }
+
+    @Override
+    public void success(List<Board> boards, Response response) {
+        for (Board board : boards){
+            Log.d("Board", board.getName());
+        }
+    }
+
+    @Override
+    public void failure(RetrofitError error) {
+        Log.d("Board Error", error.getCause().toString());
     }
 }
